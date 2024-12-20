52669cbf00c6ec428cde465980ba574bf40b6c33
RafaelZelak <rzf1503@gmail.com>
2024-12-20 14:15:57 -0300

    Removed all files except .git

--- main.py
+++
@@ -1,4 +0,0 @@
-from src.ui.main import run_app
-
-if __name__ == "__main__":
-    run_app()
--- src/utils/capture.py
+++
@@ -1,6 +0,0 @@
-from PIL import ImageGrab
-
-def capture_screen():
-    # Captura a tela inteira
-    screen = ImageGrab.grab()
-    return screen
